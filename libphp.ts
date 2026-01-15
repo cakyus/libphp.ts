71f828407051fbacfe1d9a3095adf2adde4de3ad
cakyus <cakyus@gmail.com>
2026-01-16 01:37:02 +0800

    add substr()

--- libphp.ts
+++ libphp.ts
@@ -11,7 +11,23 @@ function strpos(haystack:string, needle:string, offset:number=0) :number|false {
   return pos;
 }
 
+// Return part of string
+
+function substr(s:string, offset:number, length:number=0) :string {
+
+  if (arguments.length == 2) {
+    length = s.length;
+  }
+
+  if (offset < 0) {
+    offset = s.length + offset
+  }
+
+  return s.substring(offset, offset + length);
+}
+
 export {
    strpos
+ , substr
 };
 
--- test.ts
+++ test.ts
@@ -3,6 +3,7 @@
 import { assertEquals } from "jsr:@std/assert";
 import {
    strpos
+ , substr
 } from "./libphp.ts";
 
 Deno.test('strpos', function() {
@@ -10,5 +11,10 @@ Deno.test('strpos', function() {
   assertEquals(strpos('foo', 'o'), 1);
   assertEquals(strpos('foo', 'b'), false);
   assertEquals(strpos('foo bar buz', 'bar'), 4);
+});
+
+Deno.test('substr', function() {
+  assertEquals(substr('foo', 0, 1), 'f');
+  assertEquals(substr('foo', 1), 'oo');
 });
 
